9404905f0fbb47af33d740902b17a762715b5050
ilia <5236131@mail.ru>
2022-01-05 18:43:29 +0300

    ,,,

--- Curve_Array_Pro_Magic_Curve/Magic_Curve/Magic_Curve_Ops.py
+++ Curve_Array_Pro_Magic_Curve/Magic_Curve/Magic_Curve_Ops.py
@@ -79,10 +79,10 @@ class MAGICCURVE_OT_mgcrv_ops(bpy.types.Operator):
             
             return {'CANCELLED'}
 
-        # except Exception as e:
+        except Exception as e:
             
-        #     ShowMessageBox("Unkown Error", "Please send me this report:", e, 'ERROR')
+            ShowMessageBox("Unkown Error", "Please send me this report:", e, 'ERROR')
             
-        #     return {'CANCELLED'}       
+            return {'CANCELLED'}       
  
         
