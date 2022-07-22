b201e329660fc4de64d14c9cc5b09545d3eb30c1
ilia <5236131@mail.ru>
2022-07-22 11:46:38 +0300

    ...

---
+++ Curve_Array_Pro_Magic_Curve/Magic_Curve/Engine/Switch_Direction.py
@@ -0,0 +1,12 @@
+import bpy  # type: ignore
+import bmesh  # type: ignore
+import mathutils  # type: ignore
+from .Classes import (
+    checker,
+    CurveData,
+)
+
+
+def recalculate_curve_manager():
+
+    pass
--- Curve_Array_Pro_Magic_Curve/Magic_Curve/Ops/Switch_Direction_Ops.py
+++ Curve_Array_Pro_Magic_Curve/Magic_Curve/Ops/Switch_Direction_Ops.py
@@ -1,36 +1,21 @@
-import bpy # type: ignore
+import bpy  # type: ignore
 from ..Engine.Errors import CancelError
-from ..Engine.General_Functions import first_step, second_step, final_step
-from ..Engine.Smooth_Curve_Old import (
-    create_curve as Smooth_create_curve, 
-    extruded_mesh_vector as Smooth_extruded_mesh_vector,
-    angle_between_vector as Smooth_angle_between_vector,
-    tilt_correction as Smooth_tilt_correction
-)
-from ..Engine.Strong_Curve import (
-    create_curve as Strong_create_curve,
-    extruded_mesh_vector as Strong_extruded_mesh_vector,
-    angle_between_vector as Strong_angle_between_vector,
-    cyclic_correction as Strong_cyclic_correction,
-    tilt_correction as Strong_tilt_correction,
-    curve_correction as Strong_curve_correction
+from ..Engine.Switch_Direction import (
+    recalculate_curve_manager
 )
 
-def manager_recalculate_curve():
-    
-    pass
- 
+
 class MAGICCURVE_OT_switch_direction(bpy.types.Operator):
-    '''Switch curve direction and recalculate to right tilt'''
+    """Switch curve direction and recalculate to right tilt"""
     bl_label = "Switch curve direction"
     bl_idname = 'magiccurve.switch_direction'
     bl_options = {'REGISTER', 'UNDO'}
         
-    def execute(self, context):
+    def execute(self, _):
  
         try:
             
-            manager_recalculate_curve()
+            recalculate_curve_manager()
         
             return {'FINISHED'}
         
@@ -42,4 +27,4 @@ class MAGICCURVE_OT_switch_direction(bpy.types.Operator):
             
         #     ShowMessageBox("Unkown Error, Please send me this report:", repr(err), 'ERROR')
             
-        #     return {'CANCELLED'}   
+        #     return {'CANCELLED'}
