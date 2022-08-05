a763b54b550bcfb7f0e255b74a6543839ad62a24
ilia <5236131@mail.ru>
2022-08-06 02:17:08 +0300

    ...

--- Curve_Array_Pro_Magic_Curve/Magic_Curve/Engine/Switch_Twist_Method.py
+++ Curve_Array_Pro_Magic_Curve/Magic_Curve/Engine/Switch_Twist_Method.py
@@ -1,11 +1,8 @@
 import bpy  # type: ignore
 import bmesh  # type: ignore
 from .Errors import CancelError, ShowMessageBox
-from .Toggle_Cyclic_Functions import (
-    toggle_cyclic,
-    angle_betw_vec,
-    z_vec,
-    create_z_arr,
+from .Switch_Twist_Method_Functions import (
+    switch_twist,
 )
 from .Switch_Direction_Functions import (
     checker,
@@ -19,6 +16,7 @@ from .Switch_Direction_Functions import (
     angle_correction,
     tilt_correction,
     twist_correction,
+    angle_betw_vec,
 )
 
 
@@ -29,17 +27,7 @@ def switch_twist_method_manager():
     merged_points_check(curve)
     points_select(curve)
 
-    if curve.data.twist_mode == 'Z_UP':
-
-        switched_curve = toggle_cyclic(curve)
-
-        bpy.ops.object.select_all(action='DESELECT')
-        switched_curve.select_set(True)
-        bpy.context.view_layer.objects.active = switched_curve
-
-        return
-
-    elif curve.data.twist_mode == 'TANGENT':
+    if curve.data.twist_mode == 'TANGENT':
 
         ShowMessageBox("Error", "Tangent twist curves are not supported", 'ERROR')
 
@@ -57,40 +45,31 @@ def switch_twist_method_manager():
     # Конвертируем в меш
     mesh_curve_duplicate = convert_to_mesh(curve_duplicate)
 
-    # Создадим массив z_vec для каждой начальной и конечной точки незамкнутого сплайна
-    z_vec_arr = create_z_arr(curve_duplicate_data[0])
-
     # Получаем массив y_vec
     y_vec_arr = ext_vec(mesh_curve_duplicate, curve_duplicate_data)
-
-    # Наполним z_vec_arr
-    z_vec_arr = z_vec(z_vec_arr, mesh_curve_duplicate, curve_duplicate_data)
     bpy.data.objects.remove(mesh_curve_duplicate, do_unlink=True)
 
-    # Меняем замкнутость
-    toggled_curve = toggle_cyclic(curve)
+    # Меняем метод скручивания
+    switched_curve = switch_twist(curve)
 
     # Дублируем кривую
-    toggled_curve_duplicate = duplicate(toggled_curve)
+    switched_curve_duplicate = duplicate(switched_curve)
 
     # Получим информацию о кривой
-    toggled_curve_duplicate_data = curve_data(toggled_curve_duplicate)
+    switched_curve_duplicate_data = curve_data(switched_curve_duplicate)
 
     # Конвертируем в меш
-    mesh_toggled_curve_duplicate = convert_to_mesh(toggled_curve_duplicate)
+    mesh_switched_curve_duplicate = convert_to_mesh(switched_curve_duplicate)
 
     # Получаем массив ext_vec
-    ext_vec_arr = ext_vec(mesh_toggled_curve_duplicate, toggled_curve_duplicate_data)
-
-    # Наполним z_vec_arr
-    z_vec_arr = z_vec(z_vec_arr, mesh_toggled_curve_duplicate, toggled_curve_duplicate_data)
-    bpy.data.objects.remove(mesh_toggled_curve_duplicate, do_unlink=True)
+    ext_vec_arr = ext_vec(mesh_switched_curve_duplicate, switched_curve_duplicate_data)
+    bpy.data.objects.remove(mesh_switched_curve_duplicate, do_unlink=True)
 
     # Получаем угол между векторами
-    angle_y_ext_arr = angle_betw_vec(y_vec_arr, ext_vec_arr, toggled_curve_duplicate_data[0], z_vec_arr)
+    angle_y_ext_arr = angle_betw_vec(y_vec_arr, ext_vec_arr, switched_curve_duplicate_data[0])
 
     # Дублируем кривую
-    test_curve = duplicate(toggled_curve)
+    test_curve = duplicate(switched_curve)
 
     # Тестово поворачиваем точки на половину угла
     tilt_correction(angle_y_ext_arr, test_curve, True)
@@ -99,23 +78,24 @@ def switch_twist_method_manager():
     mesh_test_curve = convert_to_mesh(test_curve)
 
     # Получаем массив test_vec
-    test_vec_arr = ext_vec(mesh_test_curve, toggled_curve_duplicate_data)
+    test_vec_arr = ext_vec(mesh_test_curve, switched_curve_duplicate_data)
     bpy.data.objects.remove(mesh_test_curve, do_unlink=True)
 
     # Получаем угол между векторами после поворота
-    angle_y_test_arr = angle_betw_vec(y_vec_arr, test_vec_arr, toggled_curve_duplicate_data[0], z_vec_arr)
+    angle_y_test_arr = angle_betw_vec(y_vec_arr, test_vec_arr, switched_curve_duplicate_data[0])
 
     # Корректируем углы
     angle_y_ext_arr = angle_correction(angle_y_ext_arr, angle_y_test_arr)
 
     # Корректируем тильт
-    tilt_correction(angle_y_ext_arr, toggled_curve, False)
+    tilt_correction(angle_y_ext_arr, switched_curve, False)
 
-    tilt_twist_ext_arr = tilt_twist_calc(toggled_curve)
+    # Корректируем направление вращения
+    tilt_twist_ext_arr = tilt_twist_calc(switched_curve)
 
     # Корректируем твист
-    twist_correction(tilt_twist_y_arr, tilt_twist_ext_arr, toggled_curve)
+    twist_correction(tilt_twist_y_arr, tilt_twist_ext_arr, switched_curve)
 
     bpy.ops.object.select_all(action='DESELECT')
-    toggled_curve.select_set(True)
-    bpy.context.view_layer.objects.active = toggled_curve
+    switched_curve.select_set(True)
+    bpy.context.view_layer.objects.active = switched_curve
---
+++ Curve_Array_Pro_Magic_Curve/Magic_Curve/Engine/Switch_Twist_Method_Functions.py
@@ -0,0 +1,16 @@
+import bpy  # type: ignore
+import bmesh  # type: ignore
+import mathutils  # type: ignore
+
+
+def switch_twist(curve):
+
+    if curve.data.twist_mode == 'Z_UP':
+
+        curve.data.twist_mode = 'MINIMUM'
+
+    else:
+
+        curve.data.twist_mode = 'Z_UP'
+
+    return curve
