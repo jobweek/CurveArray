fe0a03a87ad0c8f6790c9513c6fc83bfd849e874
ilia <5236131@mail.ru>
2022-08-31 17:43:42 +0300

    ...

---
+++ Curve_Array_Magic_Curve/Curve_Array/Panels/General_Panel.py
@@ -0,0 +1,13 @@
+import bpy  # type: ignore
+
+
+class CURVEARRAY_PT_general_panel(bpy.types.Panel):
+
+    bl_label = 'CurveArray'
+    bl_idname = 'CURVEARRAY_PT_general_panel'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'CurveArray'
+
+    def draw(self, context):
+        pass
