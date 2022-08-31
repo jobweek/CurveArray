4d7f9d8a3026f50c7bf469d8e3fbdc8abf78fb2c
ilia <5236131@mail.ru>
2022-08-31 17:45:47 +0300

    ...

--- Curve_Array_Magic_Curve/Curve_Array/Panels/General_Panel.py
+++ Curve_Array_Magic_Curve/Curve_Array/Panels/General_Panel.py
@@ -3,7 +3,7 @@ import bpy  # type: ignore
 
 class CURVEARRAY_PT_general_panel(bpy.types.Panel):
 
-    bl_label = 'CurveArray'
+    bl_label = 'Curve Array'
     bl_idname = 'CURVEARRAY_PT_general_panel'
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
--- Curve_Array_Magic_Curve/Magic_Curve/Panels/Main_Panel.py
+++ Curve_Array_Magic_Curve/Magic_Curve/Panels/Main_Panel.py
@@ -1,12 +1,13 @@
 import bpy  # type: ignore
 
 
-class MAGICCURVE_PT_main_panel(bpy.types.Panel):
+class MAGICCURVE_PT_general_panel(bpy.types.Panel):
+
     bl_label = "Magic Curve"
-    bl_idname = "MAGICCURVE_PT_main_panel"
+    bl_idname = "MAGICCURVE_PT_general_panel"
     bl_space_type = 'VIEW_3D' 
     bl_region_type = 'UI'
-    bl_category = 'CrvArrPro' 
+    bl_category = 'MagicCurve'
 
     def draw(self, _):
 
